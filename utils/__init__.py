289f44bf83aa67e548b725d5438e009d8adb47cc
lzzlovetbb <20230608lzz@gmail.com>
2026-09-04 23:29:57 +0800

    完善评估模块与提交文档

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,5 @@
 """Evaluation metrics and model interpretation utilities."""
 
-from .metrics import evaluate_model, topk_accuracy
+from .metrics import evaluate_model, plot_normalized_confusion_matrix, topk_accuracy
 
-__all__ = ["evaluate_model", "topk_accuracy"]
-
+__all__ = ["evaluate_model", "plot_normalized_confusion_matrix", "topk_accuracy"]
